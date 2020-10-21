58d7b60c6bcadf0d59126bfcffad44735b46361e
Tobias Junghans <tobydox@veyon.io>
2020-10-21 19:28:39 +0200

    CI: disable Wayland support for old distros

--- .ci/linux.centos.7.8/script.sh
+++ .ci/linux.centos.7.8/script.sh
@@ -4,5 +4,7 @@ source scl_source enable devtoolset-7
 
 set -e
 
+export CMAKE_FLAGS="-DWITH_WAYLAND_SUPPORT=OFF"
+
 $1/.ci/common/linux-build.sh $1 $2
 $1/.ci/common/finalize-rpm.sh $1 $2 "centos-7.8"
--- .ci/linux.debian.stretch/script.sh
+++ .ci/linux.debian.stretch/script.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export CMAKE_FLAGS="-DWITH_WAYLAND_SUPPORT=OFF"
+
 $1/.ci/common/linux-build.sh $1 $2
 $1/.ci/common/finalize-deb.sh $1 $2 "debian-stretch"
 
